987c2c263a98b746a55f7ef4080fa0e9e3da1a88
Quadart21 <108038976+Quadart21@users.noreply.github.com>
2025-05-25 19:54:48 +0400

    Update config.py

--- config.py
+++ config.py
@@ -12,13 +12,12 @@ logger = logging.getLogger(__name__)
 
 
 # Токен бота
-BOT_TOKEN = "7850744684:AAFyveu4x5d883UXWgQRW4TBdjRI0fu89Ow"
-CHANNEL_ID = -1002422011698  # Замените на ваш ID группы
-ADMIN_ID = "8058997213"  # Используем список чисел
-NOWNODES_API_KEY = "f28bec03-0b48-4d22-a88e-ba3249d2f3ab"
+BOT_TOKEN = ""
+ADMIN_ID = ""  # Используем список чисел
+NOWNODES_API_KEY = ""
 HELEKET_API_URL = "https://api.heleket.com/v1/payment"
-HELEKET_MERCHANT_ID = "f62d2ac5-addd-4d28-8ac9-c7ebe89d6ef3"
-HELEKET_API_KEY = "75ccYNzG0DDsSGxm7qLSjsZyjYZXSXGv0IYSwf7bkssEsnODg9EchjKMa7qH3kEACxFjhGAKbgFT6XCE6vjgdw5iCH1XfVybChIZ5Xcf5KLqsPc2Vp4Vo9uaGtbgMLxX"
+HELEKET_MERCHANT_ID = ""
+HELEKET_API_KEY = ""
 # Инициализация бота и хранилища FSM
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
